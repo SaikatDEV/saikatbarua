5ed24c45816607d455b6dd9150e39eee9ea0660a
SaikatDev <saikat.b965@gmail.com>
2025-02-15 14:10:34 -0500

    updated footer and added connect page

--- src/components/Home/LatestWorks.jsx
+++ src/components/Home/LatestWorks.jsx
@@ -25,6 +25,14 @@ function LatestWorks() {
     }
   };
 
+  // Scroll to the Lets connect
+  const scrollToletsConnect = () => {
+    const section = document.getElementById("letsConnect");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
       {/**This is the linkedin, github, stackoverflow section */}
@@ -394,12 +402,100 @@ function LatestWorks() {
           className="text-2xl bg-gradient-to-l from-[#1595b6] to-[#1f2667e6] 
         hover:bg-gray-100 font-semibold py-2 px-4 border-none rounded-md 
         text-white transform transition duration-200 hover:scale-110
-        left-1/2 -translate-x-1/2 absolute bg-[#111] sm:bottom-3"
+        left-1/2 -translate-x-1/2 absolute bg-[#111] bottom-3"
           // style={{ bottom: "12px" }}
-          onClick={scrollToLatestWorks}
+          onClick={scrollToletsConnect}
         >
           Let's Connect
         </button>
+      </section>
+      {/** This is the Lets Connect Section */}
+      <section
+        id="letsConnect"
+        class="bg-[#111939] sm:px-10 md:px-32 lg:px-44 h-[64vh] flex flex-col md:flex-row justify-center items-center"
+      >
+        <div class="w-full mt-4">
+          <img src="/src/assets/images/connect.png" alt="connect" />
+        </div>
+        <div class="w-full mb-10">
+          <form class="max-w-sm mx-auto">
+            <div class="mt-4">
+              <label
+                for="username-icon"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Name
+              </label>
+              <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                  </svg>
+                </div>
+                <input
+                  type="text"
+                  id="username-icon"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your name here"
+                ></input>
+              </div>
+            </div>
+            <div class="mt-4">
+              <label
+                for="email-address-icon"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Email
+              </label>
+              <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                  <svg
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 16"
+                  >
+                    <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                    <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                  </svg>
+                </div>
+                <input
+                  type="text"
+                  id="email-address-icon"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="youremail@email.com"
+                ></input>
+              </div>
+            </div>
+            <div class="mt-4">
+              <label
+                for="message"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </section>
     </>
   );
