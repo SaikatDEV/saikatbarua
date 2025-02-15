6bdbd053f57bd26e0f75a27d54686cc163ec066b
SaikatDev <saikat.b965@gmail.com>
2025-02-15 15:08:44 -0500

    Removed unused links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -36,17 +36,7 @@ export default function Footer() {
             <ul class="text-gray-500 dark:text-gray-400 font-medium">
               <li class="mb-4">
                 <a href="#" class="hover:underline">
-                  Discord Server
-                </a>
-              </li>
-              <li class="mb-4">
-                <a href="#" class="hover:underline">
                   Twitter
-                </a>
-              </li>
-              <li class="mb-4">
-                <a href="#" class="hover:underline">
-                  Facebook
                 </a>
               </li>
               <li class="mb-4">
