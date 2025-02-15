e7adeafba399d05e9e54cd1c9c806606cafaef6b
SaikatDev <saikat.b965@gmail.com>
2025-02-15 16:11:18 -0500

    Updated Form label

--- src/components/Home/LatestWorks.jsx
+++ src/components/Home/LatestWorks.jsx
@@ -424,6 +424,7 @@ function LatestWorks() {
               <label
                 for="username-icon"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                name="name"
               >
                 Your Name
               </label>
@@ -451,6 +452,7 @@ function LatestWorks() {
               <label
                 for="email-address-icon"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                name="email"
               >
                 Your Email
               </label>
@@ -479,6 +481,7 @@ function LatestWorks() {
               <label
                 for="message"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                name="message"
               >
                 Your message
               </label>
@@ -491,9 +494,12 @@ function LatestWorks() {
             </div>
             <button
               type="submit"
-              class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+              focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full 
+              sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+              dark:focus:ring-blue-800"
             >
-              Submit
+              Send
             </button>
           </form>
         </div>
